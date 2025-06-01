a1e6f2bbc706254e04f603c3772947993feddc51
Ankit Srivastav <your.email@example.com>
2025-06-01 11:37:20 +0530

    basic backend functionality is completed

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
         trim: true,
         minlength: [3, 'Username must be at least 3 characters long']
     },
+    
     email: {
         type: String,
         required: [true, 'Email is required'],
