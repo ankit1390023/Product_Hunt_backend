d1b7e7584e1a309797b498650904953afa17d083
Ankit Srivastav <your.email@example.com>
2025-06-06 15:34:49 +0530

    URL changed

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "https://product-hunt-frontend.vercel.app",
+    origin: ["https://product-hunt-frontend.vercel.app"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
